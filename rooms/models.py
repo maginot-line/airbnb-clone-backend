94ae5c24c544ba947617a7700f77f34ce1dd8e76
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-20 14:06:11 +0900

    6.4 Rooms Admin

--- rooms/models.py
+++ rooms/models.py
@@ -8,6 +8,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = "shared_room", "Shared Room"
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -22,9 +23,18 @@ class Room(CommonModel):
         "rooms.Amenity",
     )
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Amenity(CommonModel):
     name = models.CharField(
         max_length=150,
     )
     description = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
