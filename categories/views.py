3740da02fd5dc8ac4f2fb69ff5a41c50e2b10428
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-21 15:31:11 +0900

    10.2 api_view

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 # Create your views here.
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse({"ok": True, "categories": all_categories})
+    return Response({"ok": True, "categories": Category.objects.all()})
