66c938afcedbaf71d0f334cb422d661c3de2273d
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-21 17:08:04 +0900

    10.8 DELETE

--- categories/views.py
+++ categories/views.py
@@ -2,6 +2,7 @@ from http.client import NOT_FOUND
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -21,7 +22,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -38,3 +39,6 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
