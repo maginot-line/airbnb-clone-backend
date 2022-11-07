57145f600ed5b69fc5443ca4ce517c1eab68eaad
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-07 17:27:50 +0900

    20.6 Github Code

--- users/views.py
+++ users/views.py
@@ -106,3 +106,10 @@ class JWTLogIn(APIView):
             return Response({"token": token})
         else:
             return Response({"error": "wrong password"})
+
+
+class GithubLogin(APIView):
+    def post(self, request):
+        code = request.data.get("code")
+        print(code)
+        return Response()
