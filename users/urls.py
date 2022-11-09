aeeebb43a9e6d4c8f5b04d80ba4cb180bb3df9ca
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-09 11:39:00 +0900

    20.11 Kakao Log In

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.Users.as_view()),
-    path("me/", views.Me.as_view()),
-    path("change-password/", views.ChangePassword.as_view()),
+    path("me", views.Me.as_view()),
+    path("change-password", views.ChangePassword.as_view()),
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
     path("github", views.GithubLogin.as_view()),
+    path("kakao", views.KakaoLogin.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
